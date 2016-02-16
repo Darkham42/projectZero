a17d5594ab286f47d7f5c60a4c097354e1164364
Darkham <thomas.picard666@gmail.com>
2016-02-16 11:23:59 +0100

    PDO pour que ça marche en localhost

--- heroesfilms/src/model/PDO/settings.ini.php
+++ heroesfilms/src/model/PDO/settings.ini.php
@@ -1,6 +1,6 @@
 ;<?php return; ?>
 [SQL]
-dbname = 21303622_prod
-host = mysql.info.unicaen.fr
-user = 21303622
-password = veemooluedayorou
+dbname = heroesFilms
+host = localhost
+user = root
+password = 
