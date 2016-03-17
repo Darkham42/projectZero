9cb417a15fed2bc86537903cb4ecdb0368b949ec
Darkham <thomas.picard666@gmail.com>
2016-03-17 21:31:48 +0100

    Infos par dessus affiche

--- heroesfilms/src/view/MainView.php
+++ heroesfilms/src/view/MainView.php
@@ -155,6 +155,7 @@ class MainView {
 	protected function galleryFilm($id, $f) {
 		$cclass = "film".$id;
 		$res = '<p class="card-title">'.self::htmlesc($f->getName()).' ('.substr(self::htmlesc($f->getDateSortie()), 0, 4).')</p>';
+		$res .= '<div class = "text"><br>'.self::htmlesc($f->getName()).'<br><br>Resume<br>BLABLABLA<br><br>Casting<br>'.self::htmlesc($f->getCasting()).'</div>';
 		$res .= '<img src="'.self::htmlesc($f->getPoster()).'" class="posterSize" alt="'.self::htmlesc($f->getName()).' poster" />';
 		$res .= '<p class="plus"><a href="'.$this->router->filmPage($id).'">More details</a></p>';
 		$res .= '</a></li>'."\n";
--- heroesfilms/src/view/poster.js
+++ heroesfilms/src/view/poster.js
@@ -1,8 +1,17 @@
-var $poster = document.getElementsByClassName("posterSize");
-for (var i = 0; i < $poster.length; i++) {
-  $poster[i].addEventListener("click", infoFilm);
+var $card = document.getElementsByClassName("cardFilms");
+
+for (var i = 0; i < $card.length; i++) {
+  $card[i].addEventListener("click", infoFilm);
 }
 
 function infoFilm(e) {
-  e.currentTarget.classList.toggle("bright"); 
+  var $poster = e.currentTarget.getElementsByClassName("posterSize")[0];
+  var $txt = e.currentTarget.getElementsByClassName("text")[0];
+  $poster.classList.toggle("bright");
+  if ($txt.style.opacity === "1") {
+    $txt.style.opacity = "0" ;
+  } else {
+    $txt.style.opacity = "1" ;
+  }
+  
 }
