9ace20f0d594e66cb3d23a7b7238bd1dd6025bd0
Senay3010 <seb.gamblin@hotmail.fr>
2016-04-02 15:53:20 +0200

    Must have : AddActor and DeleteActor in form
    Vu que les champs doivent être obligatoirement remplis, fallait pour les
    supprimer

--- heroesfilms/src/view/addActorForm.php
+++ heroesfilms/src/view/addActorForm.php
@@ -3,16 +3,35 @@
   var counter = 4;
   var limit = 9;
   function addInput(divName){
+       if (counter == 4) {
+          document.getElementById("delete").style.display = "inline";
+       }
        if (counter == limit)  {
             alert("You have reached the limit of inputs.");
             document.getElementById("creer").style.display = "none";
+            
        }
        else {
+            document.getElementById("delete").style.display = "inline";
             var newdiv = document.createElement('div');
             newdiv.classList.add('form-group');
             newdiv.innerHTML =  '<input id="actor'+(counter)+'" spellcheck=false class="form-control" name="casting[]" type="text" alt="film"required=""><span class="form-highlight"></span><span class="form-bar"></span><label for="actor1" class="float-label">Actor ' + (counter) + ' :</label>';
             document.getElementById(divName).appendChild(newdiv);
             counter++;
+       }
+  }
+
+  function deleteInput(divName){
+       if (counter == 4)  {
+            alert("You have reached the limit of inputs.");
+            document.getElementById("delete").style.display = "none";
+            document.getElementById("creer").style.display = "inline";
+       }
+       else {
+            document.getElementById("creer").style.display = "inline";
+            var list = document.getElementById('dynamicInput'), item = list.lastElementChild;
+            list.removeChild(item);
+            counter--;
        }
   }
 
@@ -44,5 +63,6 @@
 
 <div class="form-group">
   <input class="buttonActor" id="creer" type="button" value="Add an actor" onClick="addInput('dynamicInput');">
+  <input class="buttonDeleteActor" id="delete" type="button" value="Delete an actor" onClick="deleteInput('dynamicInput');">
   <br>
 </div>
